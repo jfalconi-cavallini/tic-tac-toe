36342fc0caed9ff7f5062b5bbe61efd9f2dbb1fc
josefalconi <jfalconi@ucsd.edu>
2023-06-08 11:47:49 -0700

    added css and some javascript

---
+++ index.js
@@ -0,0 +1,24 @@
+let playerText = document.getElementById('playerText')
+let restartBtn = document.getElementById('restartBtn')
+let boxes = Array.from(document.getElementsByClassName('box'))
+
+const O_text = "O"
+const X_text = "X"
+let currentPlayer = X_text
+let spaces = Array(9).fill(null)
+
+const startGame = () => {
+    boxes.forEach( box => box.addEventListener('click', boxClicked))
+}
+
+function boxClicked(e) {
+    const id = e.target.id
+    if(!spaces[id]){
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+
+        currentPlayer = currentPlayer == X_text ? O_text : X_text
+    }
+}
+
+startGame()
